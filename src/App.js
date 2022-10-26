a1da06b0710a964b6f1fc5d22dbba33fc78d11e1
Pooja8Lekshmi8J <116712279+Pooja8Lekshmi8J@users.noreply.github.com>
2022-10-26 16:06:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        How to deploy reactjs for free?
         </p>
         <a
           className="App-link"
